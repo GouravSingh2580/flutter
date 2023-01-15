bb26fa78eedd843ad4fb1dc50a9ad9dd9c8ced39
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2023-01-16 01:36:08 +0530

    Create main.dart

---
+++ 1/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:day1/homepage.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const Home(),
+    );
+  }
+}
